e02109ec8274a40a407f8dcb8da77ce22b3bef72
moysez <moysez@gmail.com>
2025-10-10 18:13:28 -0300

    Update upload.py

--- app/models/upload.py
+++ app/models/upload.py
@@ -8,5 +8,5 @@ class Upload(Base):
     id = Column(Integer, primary_key=True, index=True)
     client_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
     filename = Column(String, nullable=False)
-    path = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    filepath = Column(String, nullable=False)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
