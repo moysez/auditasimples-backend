8c9713fce880c36dc38f499f01c81e1bc072d444
moysez <moysez@gmail.com>
2025-10-10 17:52:51 -0300

    Update uploads.py

--- app/routers/uploads.py
+++ app/routers/uploads.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 import os, traceback
 
 from ..db import get_session
-from ..models.upload import Uploads
+from ..models.upload import Upload
 
 router = APIRouter(
     prefix="/uploads",
