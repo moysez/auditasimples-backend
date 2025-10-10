c34605f4f5ed338682edaec7a9328c61953b2f9b
moysez <moysez@gmail.com>
2025-10-10 10:31:24 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -2,16 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-# Conexão com PostgreSQL
+# Conexão com PostgreSQL (psycopg)
 DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,
+    pool_pre_ping=True,  # mantém a conexão viva
     future=True
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True
+)
 
 Base = declarative_base()
 
