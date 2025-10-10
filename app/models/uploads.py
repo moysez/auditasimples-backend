13ffda9b0533c8d168e587c11ed5f5945e490847
moysez <moysez@gmail.com>
2025-10-10 17:17:01 -0300

    Update uploads.py

--- app/models/uploads.py
+++ app/models/uploads.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String, LargeBinary, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.sql import func
 from ..db import Base
 
 class Upload(Base):
     __tablename__ = "uploads"
+
     id = Column(Integer, primary_key=True, index=True)
-    company_id = Column(Integer, ForeignKey("companies.id", ondelete="CASCADE"), nullable=False)
-    original_name = Column(String(255), nullable=False)
-    zip_data = Column(LargeBinary, nullable=False)
+    client_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
+    filename = Column(String, nullable=False)
+    path = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
