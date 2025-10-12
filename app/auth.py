f10b296902833536fad6e45c4a91768151c609e4
moysez <moysez@gmail.com>
2025-10-12 01:46:21 -0300

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -7,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
 
-ef get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme)):
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
