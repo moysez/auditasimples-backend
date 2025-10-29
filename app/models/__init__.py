f8893c40e9e914026d0cb61b3d0d2e03ca6e9655
moysez <moysez@gmail.com>
2025-10-29 00:40:23 -0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,12 @@ from .upload import Upload
 from .company import Company
 from .clients import Client
 from .user import User
-from .nfe import NFe
 from .reports import Report
 
+__all__ = [
+    "Upload",
+    "Company",
+    "Client",
+    "User",
+    "Report",
+]
