d0b2a0709c494f8c8785a57910e744e0f80e34b9
moysez <moysez@gmail.com>
2025-10-30 06:42:47 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://auditassimples.io",
+    "https://www.auditassimples.io",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 import logging
 from fastapi.responses import JSONResponse
 from app.db import Base, engine
