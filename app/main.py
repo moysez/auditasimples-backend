66e76a408b1cc5cbb06b7ea9ab255da35a503e89
moysez <moysez@gmail.com>
2025-11-04 15:19:28 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from app.routers import auth
+from app.routers import auth, uploads, dashboard, dictionary, clients, company
 
 # ============================================================
 # 🚀 CRIAÇÃO DO APP
